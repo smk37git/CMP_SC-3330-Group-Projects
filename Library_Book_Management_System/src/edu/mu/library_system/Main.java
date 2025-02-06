dfd43983365c26d3256e068e8531e9aaca51c2dc
smk37git <sdmain3@gmail.com>
2025-02-06 11:52:19 -0600

    Spacing Format Updated

--- Library_Book_Management_System/src/edu/mu/library_system/Main.java
+++ Library_Book_Management_System/src/edu/mu/library_system/Main.java
@@ -8,15 +8,19 @@ public class Main {
 		// Sebastian Main, Jase DeCota, Greg Gomez - CMP_SC-3330
 		
 		/** Test Case 1
+		 * 
 		 * @parameter There are no given parameters
 		 * @return should be "Unknown" due to no given parameters and the default parameter being "Unknown"
+		 * 
 		 */
 		Book book = new Book();
 		System.out.println("Title: " + book.getTitle());
 		
 		/** Test Case 2
+		 * 
 		 * @parameter Title = "Diary of a Wimpy Kid", Author = "Jeff Kiney", ISBN = "123", Price = "60.00"
 		 * @return Only Title is selected, return will be book title aka "Diary of a Wimpy Kid"
+		 * 
 		 */
 		Book bookWithTitle = new Book("Diary of a Wimpy Kid", "Jeff Kiney", "123", 60.00);
 		System.out.println("Title: " + bookWithTitle.getTitle());

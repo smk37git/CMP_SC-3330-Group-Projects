f46afef81ce614661f78d811af73775d02b0ad38
smk37git <sdmain3@gmail.com>
2025-02-21 17:02:07 -0600

    Created Subclasses + imported abstract methods
    Created the 5 subclasses and imported abstract methods

---
+++ Middle_Earth_Madness/src/middle_earth/subclasses/Elf.java
@@ -0,0 +1,22 @@
+package middle_earth.subclasses;
+
+import middle_earth.charactercreator.MiddleEarthCharacter;
+
+public class Elf extends MiddleEarthCharacter {
+
+	
+	// Attack Method
+	@Override
+	public void attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// Get Race Method
+	@Override
+	public void getRace() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

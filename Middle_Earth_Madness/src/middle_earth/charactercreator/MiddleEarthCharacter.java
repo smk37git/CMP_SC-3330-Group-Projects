583e688e1db50ab376fd8bd463981f889e272728
smk37git <sdmain3@gmail.com>
2025-02-21 16:35:39 -0600

    Created Middle Earth Madness Assignment
    Created our files for homework Assignment 2 -- "Middle Earth Madness"

---
+++ Middle_Earth_Madness/src/middle_earth/charactercreator/MiddleEarthCharacter.java
@@ -0,0 +1,6 @@
+package middle_earth.charactercreator;
+
+public abstract class MiddleEarthCharacter {
+	// Must extend MiddleEarthCharacter and implement attack(MiddleEarthCharacter) and getRace():
+
+}
---
+++ Middle_Earth_Madness/src/middle_earth/main/Main.java
@@ -0,0 +1,10 @@
+package middle_earth.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Homework Assignment 2 -- Sebastian Main, Jase DeCota, Greg Gomez
+
+	}
+
+}

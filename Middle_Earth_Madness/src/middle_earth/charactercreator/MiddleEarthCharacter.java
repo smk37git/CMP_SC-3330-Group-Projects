4f1dc076d55d6f1013cba2564bf893b6bf8abac0
smk37git <sdmain3@gmail.com>
2025-02-21 16:45:33 -0600

    Created Attributes for MiddleEarthCharater.java and a constructor
    Character Fields and Constructor for MiddleEarthCharacter.java

--- Middle_Earth_Madness/src/middle_earth/charactercreator/MiddleEarthCharacter.java
+++ Middle_Earth_Madness/src/middle_earth/charactercreator/MiddleEarthCharacter.java
@@ -2,5 +2,17 @@ package middle_earth.charactercreator;
 
 public abstract class MiddleEarthCharacter {
 	// Must extend MiddleEarthCharacter and implement attack(MiddleEarthCharacter) and getRace():
+	
+	// Character Fields
+	private String name;
+	private double health;
+	private double power;
+	
+	// Constructor to initialize name, health, and power
+	public MiddleEarthCharacter() {
+		this.name = "Unknown";
+		this.health = 100;
+		this.power = 0;
+	}
 
 }

92defa53f86bb014a76b8f08ecb67d9ff346406d
smk37git <sdmain3@gmail.com>
2025-02-21 16:56:16 -0600

    Created two abstract methods for MiddleEarthCharacter.java (attack / getRace)
    These two methods are abstract to be used later.
    
    Attack will reduce a targets health if successful. Otherwise the target does not lose health.
    
    getRace simply get's the character race. Races will be built out in the future.

--- Middle_Earth_Madness/src/middle_earth/charactercreator/MiddleEarthCharacter.java
+++ Middle_Earth_Madness/src/middle_earth/charactercreator/MiddleEarthCharacter.java
@@ -14,5 +14,20 @@ public abstract class MiddleEarthCharacter {
 		this.health = 100;
 		this.power = 0;
 	}
+	
+	/**
+	 * Abstract Attack Method
+	 * @param target
+	 * 
+	 * If true, reduce target health
+	 * if false OR target is same kin, do NOT reduce target health
+	 */
+	public abstract void attack(MiddleEarthCharacter target);
+	
+	/**
+	 * Abstract getRace Method
+	 * Return's the race of a character
+	 */
+	public abstract void getRace();
 
 }

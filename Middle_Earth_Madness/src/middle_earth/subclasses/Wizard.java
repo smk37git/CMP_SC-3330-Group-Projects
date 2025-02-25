5ca7154af5f1164f4f25b0ddebecdbabf3b9264a
smk37git <sdmain3@gmail.com>
2025-02-25 13:42:17 -0600

    Finished adding Attack Method!

--- Middle_Earth_Madness/src/middle_earth/subclasses/Wizard.java
+++ Middle_Earth_Madness/src/middle_earth/subclasses/Wizard.java
@@ -8,17 +8,49 @@ public class Wizard extends MiddleEarthCharacter {
 	// Attack Method
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
+		// 1.5x damage to Dwarf
+		// 1x damage to Human and Wizard
+		// 0x (Zero DMG) damage to Elf and Orc
+
+		// Attack Value / Damage Values
+		double attack = attackValue();
+		double damage;
 		
+		// Get the race of the target
+		String targetRace = target.getRace();
 		
-		return false;
+		if(targetRace.equals("Dwarf")) {
+			damage = attack * 1.5;
+			target.setHealth(target.getHealth() - damage);
+			return true;
+			
+		} else if(targetRace.equals("Human") || targetRace.equals("Wizard")) {
+			damage = attack * 1;
+			target.setHealth(target.getHealth() - damage);
+			return true;
+			
+		} else if(targetRace.equals("Elf") || targetRace.equals("Orc")) {
+			damage = attack * 0;
+			target.setHealth(target.getHealth() * damage);
+			return true;
+			
+		}else {
+			return false;
+		}
+		
 	}
 
 	// Get Race Method
 	@Override
-	public void getRace() {
-		// TODO Auto-generated method stub
+	public String getRace() {
+		return "Wizard";
+	}
+	
+	// Attack Method
+	private double attackValue() {
 		
+		// Base DMG value is at 10
+		return 10.0;
 	}
 
 }

0d590269fa794c6a4bd6b167a51815791df6ee47
gregg <greggomez2004@gmail.com>
2025-03-01 21:06:11 -0600

    started work on CharacterManager fields and methods

--- Middle_Earth_Madness/src/middle_earth/charactermanagementsystem/CharacterManager.java
+++ Middle_Earth_Madness/src/middle_earth/charactermanagementsystem/CharacterManager.java
@@ -1,5 +1,18 @@
 package middle_earth.charactermanagementsystem;
 
+import middle_earth.charactercreator.MiddleEarthCharacter;
+
 public class CharacterManager {
 
+	//Fields 
+	private MiddleEarthCharacter[] characters;
+	private int size;
+	
+	//methods
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		int i;
+		for (i = 0; i < size; i++) {
+			
+		}
+	}
 }

e3cdc319077c2ec9e55d6d7af2bc5af203bce868
gregg <greggomez2004@gmail.com>
2025-03-02 14:06:21 -0600

    finished addCharacter method in CharacterManager

--- Middle_Earth_Madness/src/middle_earth/charactermanagementsystem/CharacterManager.java
+++ Middle_Earth_Madness/src/middle_earth/charactermanagementsystem/CharacterManager.java
@@ -5,14 +5,28 @@ import middle_earth.charactercreator.MiddleEarthCharacter;
 public class CharacterManager {
 
 	//Fields 
-	private MiddleEarthCharacter[] characters;
+	private MiddleEarthCharacter[] characters = new MiddleEarthCharacter[2];
 	private int size;
 	
 	//methods
+	//adds character c to characters array. doubles array length if full, returns true if necessary
 	public boolean addCharacter(MiddleEarthCharacter c) {
 		int i;
-		for (i = 0; i < size; i++) {
-			
+		for (i = 0; i < characters.length; i++) {
+			if (characters[i] == null) {
+				characters[i] = c;
+				size++;
+			}
 		}
+		if (size == characters.length) {
+			MiddleEarthCharacter[] characters2 = new MiddleEarthCharacter[characters.length*2];
+			int b;
+			for (b=0; b < characters.length; b++) {
+				characters2[b] = characters[b];
+			}
+			characters = characters2;
+			return true;
+		}
+		return false;
 	}
 }

adf8893add294d84dd56bbd955e851cfa39cd856
smk37git <sdmain3@gmail.com>
2025-04-16 17:15:47 -0500

    Assigned cells; added getters and setters, and generated toString

--- SP2025_CS3330_HW4_G17/src/hw4/maze/Row.java
+++ SP2025_CS3330_HW4_G17/src/hw4/maze/Row.java
@@ -1,7 +1,28 @@
 package hw4.maze;
 
+import java.util.ArrayList;
+
 public class Row {
 	
 	private ArrayList<Cell> cells;
+	
+	/* Assign Cells */
+	public Row(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
 
+	/* Getters and Setter */
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+	
+	/* To String */
+	@Override
+	public String toString() {
+		return "Row [cells=" + cells + "]";
+	}
 }

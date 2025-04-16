7c96c156bb3b23c704cd690a18f5032ef4c34ac4
smk37git <sdmain3@gmail.com>
2025-04-16 17:18:37 -0500

    Finished Grid Class; assigned, getters and setters, toString. hw4.maze.test package files now pass JUnit Testing

--- SP2025_CS3330_HW4_G17/src/hw4/maze/Grid.java
+++ SP2025_CS3330_HW4_G17/src/hw4/maze/Grid.java
@@ -7,4 +7,23 @@ public class Grid {
 	/* Grid Class Row */
 	private ArrayList<Row> rows; 
 
+	/* Assign Rows */
+	public Grid(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+	
+	/* Getters and Setters */
+	public ArrayList<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(ArrayList<Row> rows) {
+		this.rows = rows;
+	}
+	
+	/* To String */
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
 }

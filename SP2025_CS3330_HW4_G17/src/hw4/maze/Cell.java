2c430f3b47d3d4cc898a6c043b260df8cfaef5f7
smk37git <sdmain3@gmail.com>
2025-04-16 16:51:51 -0500

    Create Cell class and added private CellComponent movement

--- SP2025_CS3330_HW4_G17/src/hw4/maze/Cell.java
+++ SP2025_CS3330_HW4_G17/src/hw4/maze/Cell.java
@@ -1,5 +1,10 @@
 package hw4.maze;
 
 public class Cell {
-
+	
+	/* Cell Class Movements */
+	private CellComponents up;
+	private CellComponents down;
+	private CellComponents left;
+	private CellComponents right;
 }
--- SP2025_CS3330_HW4_G17/src/hw4/maze/CellComponents.java
+++ SP2025_CS3330_HW4_G17/src/hw4/maze/CellComponents.java
@@ -1,5 +1,7 @@
 package hw4.maze;
 
 public enum CellComponents {
+	
+	/* Defined Enums for Different Locations */
 	WALL, APERTURE, EXIT
 }

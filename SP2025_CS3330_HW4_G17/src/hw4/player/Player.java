0ba9259cf28df42c1f72a9ee8a75668c4c584e6d
gregg <greggomez2004@gmail.com>
2025-04-19 15:18:57 -0500

    added javadoc to Player

--- SP2025_CS3330_HW4_G17/src/hw4/player/Player.java
+++ SP2025_CS3330_HW4_G17/src/hw4/player/Player.java
@@ -5,9 +5,15 @@ import hw4.maze.Row;
 
 public class Player {
 	
+	/*
+	 * Agent row and cell
+	 */
 	private Row row;
 	private Cell cell;
 
+	/*
+	 * Current agent row and cell
+	 */
 	public Player(Row row, Cell cell) {
 		// TODO Auto-generated constructor stub
 		this.row = row;
@@ -15,22 +21,37 @@ public class Player {
 		
 	}
 	
+	/*
+	 * To string
+	 */
 	public String toString() {
 		return "Player [currentCell=" + cell + ", currentRow=" + row + "]";
 	}
 	
+	/*
+	 * Gets agent's current Row
+	 */
 	public Row getCurrentRow() {
 		return row;
 	}
 	
+	/*
+	 * Sets agent's current Row
+	 */
 	public void setCurrentRow(Row row) {
 		this.row = row;
 	}
 	
+	/*
+	 * Gets agent's current Cell
+	 */
 	public Cell getCurrentCell() {
 		return cell;
 	}
 	
+	/*
+	 * Sets agent's current Cell
+	 */
 	public void setCurrentCell(Cell cell) {
 		this.cell = cell;
 	}

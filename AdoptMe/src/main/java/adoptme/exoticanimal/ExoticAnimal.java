1f0f19187a28dd8fb37e4153cef486db424042b2
smk37git <sdmain3@gmail.com>
2025-04-27 17:16:11 -0500

    Created ExoticAnimal Class; added attributes; added getters and setters; generated toString()

---
+++ AdoptMe/src/main/java/adoptme/exoticanimal/ExoticAnimal.java
@@ -0,0 +1,60 @@
+package adoptme.exoticanimal;
+
+public class ExoticAnimal {
+	
+	// Attributes
+	private String uniqueId;
+	private String animalName;
+	private String category;
+	private String subSpecies;
+	private int yearsOld;
+	
+	// Getters and Setters
+	
+	// Get / Set ID
+	public String getUniqueId() {
+		return uniqueId;
+	}
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+	
+	// Get / Set Name
+	public String getAnimalName() {
+		return animalName;
+	}
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+	
+	// Get / Set Category
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	// Get / Set Species
+	public String getSubSpecies() {
+		return subSpecies;
+	}
+	public void setSubSpecies(String subSpecies) {
+		this.subSpecies = subSpecies;
+	}
+	
+	// Get / Set Age
+	public int getYearsOld() {
+		return yearsOld;
+	}
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+	
+	// To String
+	@Override
+	public String toString() {
+		return "ExoticAnimal [uniqueId=" + uniqueId + ", animalName=" + animalName + ", category=" + category
+				+ ", subSpecies=" + subSpecies + ", yearsOld=" + yearsOld + "]";
+	}
+}
